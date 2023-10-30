555060c216f146178925bae852711c9b6cfd8861
evergreen-96 <89900095+evergreen-96@users.noreply.github.com>
2023-10-30 23:13:06 +0500

    убрал ошибки JS на qantity: переписал вызов модала на listener. Добавил проверку пустого поля

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['192.168.0.108', '127.0.0.1', 'localhost']
 
 # Application definition
-APPEND_SLASH = False
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
