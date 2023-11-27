d0e883185502f8a2a2b5156c78eb41511bb31045
evergreen-96 <89900095+evergreen-96@users.noreply.github.com>
2023-11-27 20:22:35 +0500

    add static and some icons

--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -13,7 +13,7 @@ fi
 python manage.py flush --no-input
 python manage.py makemigrations core
 python manage.py migrate
-
+python manage.py collectstatic --no-input
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
 python manage.py loaddata initial_data.json
 exec "$@"
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -121,11 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'core/static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': lambda request: DEBUG,
